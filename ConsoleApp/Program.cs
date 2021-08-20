8d23a2b2a2a0d7f102c697c1e25a9d69a8f804a8
Sa'di <88400372+Saadi0101@users.noreply.github.com>
2021-08-21 01:35:27 +0500

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        class Rectangle
+        {
+            public double side1;
+            public double side2;
+
+            public Rectangle ()
+            {
+                Console.WriteLine("Введиту длину 1 сторону прямоугольника: ");
+                side1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введиту длину 2 сторону прямоугольника: ");
+                side2 = double.Parse(Console.ReadLine());
+            }
+
+            public double AreaCalculator()
+            {
+                return side1 * side2;
+            }
+
+            public double PerimeterCalculator()
+            {
+                return 2 * (side1 + side2);
+            }
+
+            public double Area { get; }
+
+            public double Perimetr { get; }
+        }
+        
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine($"Площадь прямоугольника: {rectangle.AreaCalculator()}");
+            Console.WriteLine($"Периметр прямоугольника: {rectangle.PerimeterCalculator()}");
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        class Book
+        {
+            public string nameBook;
+
+            public void Show()
+            {
+                Console.Write($"Название киги: ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(nameBook);
+                Console.ResetColor();
+            }
+
+        }
+
+        class Title 
+        {
+            public string nameTitle;
+
+            public void Show()
+            {
+                Console.Write($"Заголовок: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(nameTitle);
+                Console.ResetColor();
+            }
+
+        }
+       
+        class Author
+        {
+            
+            public string nameAuthor;
+
+            public void Show()
+            {
+                Console.Write($"Автор: ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(nameAuthor);
+                Console.ResetColor();
+            }
+
+        }
+        
+        class Content
+        {
+            public string content;
+
+            public void Show()
+            {
+                Console.Write($"Содержание: ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(content);
+                Console.ResetColor();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Title title = new Title();
+            Author author = new Author();
+            Content content = new Content();
+
+            Console.WriteLine("Введите название книги: ");
+            book.nameBook = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите заголовку: ");
+            title.nameTitle = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите имя автора: ");
+            author.nameAuthor = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите содержание книги: ");
+            content.content = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+
+            book.Show();
+            title.Show();
+            author.Show();
+            content.Show();
+        }
+    }
+}
